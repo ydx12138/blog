dd31b3e00ce083060c7eb6bc6b142638f7bdcaa5
杨栋旭 <2675723732@qq.com>
2026-06-17 22:02:20 +0800

    封装响应体，自定义响应码，首页查询文章接口，dto:articlesimple

--- internal/dao/article.go
+++ internal/dao/article.go
@@ -2,7 +2,6 @@ package dao
 
 import (
 	"blog/core"
-	"blog/models"
 	"blog/models/dto"
 
 	"go.uber.org/zap"
@@ -11,7 +10,29 @@ import (
 // 按页码获取10条已发布的文章
 func GetArticleByPage(page int) ([]dto.ArticleSimple, error) {
 	var articleList []dto.ArticleSimple = make([]dto.ArticleSimple, 0)
-	err := core.DB.Model(models.Article{}).Preload("Category").Limit(10).Offset((page-1)*10).Find(&articleList, "status = ?", 2).Error
+	//err := core.DB.Model(models.Article{}).Preload("Category").Limit(10).Offset((page-1)*10).Find(&articleList, "status = ?", 2).Error
+	err := core.DB.
+		Table("article a").
+		Select(`
+			a.id,
+			a.title,
+			a.summary,
+			a.cover,
+			a.category_id,
+			c.name AS category_name,
+			a.view_count,
+			a.like_count,
+			a.comment_count,
+			a.tags,
+			a.created_at,
+			a.updated_at
+		`).
+		Joins("LEFT JOIN category c ON a.category_id = c.id").
+		Where("a.status = ?", 2).
+		Limit(10).
+		Offset((page - 1) * 10).
+		Scan(&articleList).Error
+
 	if err != nil {
 		zap.L().Error("GetArticleByPage()" + err.Error())
 		return nil, err
--- models/dto/articleSimple.go
+++ models/dto/articleSimple.go
@@ -1,7 +1,6 @@
 package dto
 
 import (
-	"blog/models"
 	"time"
 )
 
@@ -10,8 +9,8 @@ type ArticleSimple struct {
 	Title        string `gorm:"size:200;not null;comment:标题" json:"title"`
 	Summary      string `gorm:"size:500;comment:摘要" json:"summary"`
 	Cover        string `gorm:"size:255;comment:封面" json:"cover"`
-	CategoryID   uint64 `json:"-" gorm:"comment:类别ID" json:"category_id"`
-	Category     models.Category
+	CategoryID   uint64 `json:"-" gorm:"comment:类别ID"`
+	CategoryName string `json:"category_name"`
 	ViewCount    uint64 `gorm:"default:0;comment:浏览数" json:"view_count"`
 	LikeCount    uint64 `gorm:"default:0;comment:点赞数" json:"like_count"`
 	CommentCount uint64 `gorm:"default:0;comment:评论数" json:"comment_count"`
